b89b8b3185fb9f0b31522ac3cb3e87e0b2df87da
Krzysztof Krzepicki <Krzysztof.Krzepicki@gmail.com>
2018-12-01 13:37:17 +0100

    Dodaj pliki projektów.

---
+++ KolokwiumOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KolokwiumOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
